56bcba8525f57d14acbd749a26c2e244381a6833
Cold-Script <166982379+Cold-Script@users.noreply.github.com>
2024-06-28 07:16:48 +0700

    Update YOU HUB.lua

--- YOU HUB.lua
+++ YOU HUB.lua
@@ -1,113 +1,214 @@
-if not game:IsLoaded() then
-    game.Loaded:Wait()
+local function billboard(child, name, name2, name3)
+	local billboard_gui = Instance.new("BillboardGui")
+	billboard_gui.Active = true
+	billboard_gui.AlwaysOnTop = true
+	billboard_gui.ClipsDescendants = true
+	billboard_gui.LightInfluence = 1
+	billboard_gui.Size = UDim2.new(10, 0, 5, 0)
+	billboard_gui.ResetOnSpawn = false
+	billboard_gui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+	billboard_gui.Parent = child
+	billboard_gui.Name = "KiwiHighlight"
+	if name2 then
+		billboard_gui.Name = "KiwiHighlight_2"
+	end
+	if name3 then
+		billboard_gui.Name = "KiwiHighlight_3"
+	end
+
+	local text_label = Instance.new("TextLabel")
+	text_label.Font = Enum.Font.Merriweather
+	text_label.Text = name
+	text_label.TextColor3 = Color3.new(1, 1, 1)
+	text_label.TextScaled = true
+	text_label.TextSize = 30
+	text_label.TextWrapped = true
+	text_label.BackgroundColor3 = Color3.new(1, 1, 1)
+	text_label.BackgroundTransparency = 1
+	text_label.BorderColor3 = Color3.new(0, 0, 0)
+	text_label.BorderSizePixel = 0
+	text_label.Size = UDim2.new(1, 0, 1, 0)
+	text_label.Visible = true
+	text_label.Parent = billboard_gui
+
+	local uistroke = Instance.new("UIStroke")
+	uistroke.Thickness = 1
+	uistroke.Parent = text_label
+
+	spawn(function()
+		while task.wait() do
+			local hue = tick() % 5 / 5
+			local color = Color3.fromHSV(hue, 1, 1) 
+			text_label.TextColor3 = color
+		end
+	end)
 end
 
-game:GetService("GuiService"):ClearError()
+local function billboard2(child, name, name2, name3)
+	local billboard_gui = Instance.new("BillboardGui")
+	billboard_gui.Active = true
+	billboard_gui.AlwaysOnTop = true
+	billboard_gui.ClipsDescendants = true
+	billboard_gui.LightInfluence = 1
+	billboard_gui.Size = UDim2.new(10, 0, 5, 0)
+	billboard_gui.ResetOnSpawn = false
+	billboard_gui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+	billboard_gui.Parent = child
+	billboard_gui.Name = "KiwiHighlight2"
+	if name2 then
+		billboard_gui.Name = "KiwiHighlight_2"
+	end
+	if name3 then
+		billboard_gui.Name = "KiwiHighlight_3"
+	end
 
-local OrionLib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Giangplay/Script/main/Orion_Library_PE_V2.lua")))()
-local Window = OrionLib:MakeWindow({IntroText = "YOU HUB | Super Box Siege Defense", IntroIcon = "rbxassetid://7733955740",Name = "YOU HUB | SUPER BOT SIEGE DEFENSE".." | YOU USE CLIENT : ".. identifyexecutor(), HidePremium = false, SaveConfig = false, IntroEnabled = true, ConfigFolder = ""})
-if _G.Ex then
-  OrionLib:Notify("Script Only 1")
-  return
+	local text_label = Instance.new("TextLabel")
+	text_label.Font = Enum.Font.Merriweather
+	text_label.Text = name
+	text_label.TextColor3 = Color3.new(1, 1, 1)
+	text_label.TextScaled = true
+	text_label.TextSize = 30
+	text_label.TextWrapped = true
+	text_label.BackgroundColor3 = Color3.new(1, 1, 1)
+	text_label.BackgroundTransparency = 1
+	text_label.BorderColor3 = Color3.new(0, 0, 0)
+	text_label.BorderSizePixel = 0
+	text_label.Size = UDim2.new(1, 0, 1, 0)
+	text_label.Visible = true
+	text_label.Parent = billboard_gui
+
+	local uistroke = Instance.new("UIStroke")
+	uistroke.Thickness = 1
+	uistroke.Parent = text_label
+
+	spawn(function()
+		while task.wait() do
+			local hue = tick() % 5 / 5
+			local color = Color3.fromHSV(hue, 1, 1) 
+			text_label.TextColor3 = color
+		end
+	end)
 end
-_G.Ex = true
-local Tab1 = Window:MakeTab({
-	Name = "Main",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+local function billboard3(child, name, name2, name3)
+	local billboard_gui = Instance.new("BillboardGui")
+	billboard_gui.Active = true
+	billboard_gui.AlwaysOnTop = true
+	billboard_gui.ClipsDescendants = true
+	billboard_gui.LightInfluence = 1
+	billboard_gui.Size = UDim2.new(10, 0, 5, 0)
+	billboard_gui.ResetOnSpawn = false
+	billboard_gui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+	billboard_gui.Parent = child
+	billboard_gui.Name = "KiwiHighlight"
+	if name2 then
+		billboard_gui.Name = "KiwiHighlight_2"
+	end
+	if name3 then
+		billboard_gui.Name = "KiwiHighlight_3"
+	end
+
+	local text_label = Instance.new("TextLabel")
+	text_label.Font = Enum.Font.Merriweather
+	text_label.Text = name
+	text_label.TextColor3 = Color3.new(1, 1, 1)
+	text_label.TextScaled = true
+	text_label.TextSize = 30
+	text_label.TextWrapped = true
+	text_label.BackgroundColor3 = Color3.new(1, 1, 1)
+	text_label.BackgroundTransparency = 1
+	text_label.BorderColor3 = Color3.new(0, 0, 0)
+	text_label.BorderSizePixel = 0
+	text_label.Size = UDim2.new(1, 0, 1, 0)
+	text_label.Visible = true
+	text_label.Parent = billboard_gui
+
+	local uistroke = Instance.new("UIStroke")
+	uistroke.Thickness = 1
+	uistroke.Parent = text_label
+
+	spawn(function()
+		while task.wait() do
+			local hue = tick() % 5 / 5
+			local color = Color3.fromHSV(hue, 1, 1) 
+			text_label.TextColor3 = color
+		end
+	end)
+end
+
+
+local repo = 'https://raw.githubusercontent.com/mstudio45/LinoriaLib/main/'
+
+local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
+local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
+local Window = Library:CreateWindow({
+	Title = 'YOU HUB | Super Crate Battle',
+	Center = true,
+	AutoShow = true,
+	Resizable = true,
+	ShowCustomCursor = true,
+	TabPadding = 8,
+	MenuFadeTime = 0.2
 })
+W = Window:AddTab("Main")
+W = W:AddLeftGroupbox("ESP")
 
-Tab1:AddSlider({Name="Hitbox Range",Min=10,Value=35,Max=35,Callback=function(TV)
-if _G.EnableHitbox then
-while task.wait() do
-spawn(function()
-	pcall(function()
-for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
-    v.HumanoidRootPart.Size = Vector3.new(TV, TV, TV)
-    v.HumanoidRootPart.Transparency = 0.85
-	   end)
-end)									
-        end
-    end
-end
-end})
-Tab1:AddToggle({Name="Enable Toggle Hitbox",Value=false,Callback=function(CAM)
-_G.EnableHitbox = CAM
-end})
-local Tab2 = Window:MakeTab({
-	Name = "Players",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+local Toggle = W:AddToggle("",{
+	Text = "ESP Enemies [Text]",
+	Callback = function(Value)
+		if Value then
+			for _, v in pairs(workspace.Enemies:GetDescendants()) do
+				if v.Name == "HumanoidRootPart" then
+					billboard(v, "Enemies")
+				end
+			end
+			ClosetESP = workspace.Enemies.ChildAdded:Connect(function(child)
+				task.wait(1)
+				for _, v in pairs(child:GetDescendants()) do
+	         if v.Name == "HumanoidRootPart" then
+					billboard(v, "Enemies")
+					end
+				end
+			end)
+		else
+				
+			ClosetESP:Disconnect()
+			for _, v in pairs(workspace.CurrentRooms:GetDescendants()) do
+				if v.Name == "KiwiHighlight" then
+					v:Destroy()
+				end
+			end
+		end
+	end
 })
-
-Tab2:AddSlider({Name="Speed Boost",Min=10,Value=50,Max=50,Callback=function(Speaker)
-if _G.EnableWS then
-while task.wait() do
-    spawn(function()
-	pcall(function()
-game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Speaker
-    end)
-end)
-end
-end
-end})
-Tab2:AddToggle({Name="Enable Toggle Speed Boost",Value=false,Callback=function(TV2)
-_G.EnableWS = TV2
-end})
-Tab2:AddSlider({Name="Field Of View",Min=10,Value=120,Max=120,Callback=function(Cam2)
-if _G.EnableFOV then
-while task.wait() do
-spawn(function()
-	pcall(function()
-workspace.CurrentCamera.FieldOfView = Cam2
-									end)
-end)
-								end
-   end
-end})
-Tab2:AddToggle({Name="Enable Toggle FOV",Value=false,Callback=function(Speaker2)
-_G.EnableFOV = Speaker2
-end})
-local Tab3 = Window:MakeTab({
-	Name = "Lighting",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+local Toggle = W:AddToggle("",{
+	Text = "ESP Player",
+	Callback = function(Value)
+		if Value then
+			for _, v in pairs(workspace.Enemies:GetDescendants()) do
+				if v.Name == game.Players.LocalPlayer.DisplayName then
+					billboard2(v, game.Players.LocalPlayer.DisplayName )
+				end
+			end
+			PlayerESP = workspace.Enemies.ChildAdded:Connect(function(child)
+				task.wait(1)
+				for _, v in pairs(child:GetDescendants()) do
+	         if v.Name == game.Players.LocalPlayer.DisplayName then
+					billboard2(v, game.Players.LocalPlayer.DisplayName)
+					end
+				end
+			end)
+		else
+				
+			PlayerESP:Disconnect()
+			for _, v in pairs(workspace.CurrentRooms:GetDescendants()) do
+				if v.Name == "KiwiHighlight2" then
+					v:Destroy()
+				end
+			end
+		end
+	end
 })
-Tab3:AddToggle({Name="No Fog",Value=false,Callback=function(TV3)
-   if TV3 then
-      game.Lighting.FogEnd = 9e9
-   else
-      game.Lighting.FogEnd = 150
-      end
-    end})
-Tab3:AddToggle({Name="No Darkness Effect",Value=false,Callback=function(Cam3)
-      if Cam3 then
-      game.Lighting.OutdoorAmbient = Color3.new(1,1,1)
-      else
-      game.Lighting.OutdoorAmbient = Color3.new(0,0,0)
-      end
-    end})
-Tab3:AddToggle({Name="Enable Screen Brightness",Value=false,Callback=function(Speaker3)
-      if Speaker3 then
-        game.Lighting.Brightness = 2
-      else
-        game.Lighting.Brightness = 0.5
-      end
-    end})
-Tab3:AddToggle({Name="No Shadow",Value=false,Callback=function(TV4)
-      if TV4 then
-        game.Lighting.GlobalShadows = false
-      else
-        game.Lighting.GlobalShadows = true
-      end
-    end})
-
-
-
-
-
-
-
 
 
 
